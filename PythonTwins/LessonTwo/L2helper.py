f83ab7ef2629224aefa6bc3c63345a5dac65d561
Benjamin Napier <napierbw@users.noreply.github.com>
2019-10-22 16:40:59 -0400

    Added the Python lesson 2 to accompany the slides

---
+++ PythonTwins/LessonTwo/L2helper.py
@@ -0,0 +1,20 @@
+#Don't touch -----------------
+import random
+import math
+x = 0
+count = 0
+num = 0
+#Don't touch -----------------
+
+#Loop
+for x in range(7):
+    print("count")
+    count = count + 1
+
+#Number multiplied
+
+#Sqrt
+print(math.sqrt(4))
+
+#MOD
+print(11 % 3)
