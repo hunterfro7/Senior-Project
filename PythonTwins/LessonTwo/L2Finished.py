488235d69453e44373a6a40adf3b1aed5278cd82
Benjamin Napier <31937804+napierbw@users.noreply.github.com>
2019-11-05 13:03:14 -0500

    Added the L2Finished.py
    L2Finished is the python file after lab lecture two has been completed.

---
+++ PythonTwins/LessonTwo/L2Finished.py
@@ -0,0 +1,33 @@
+
+#Don't touch -----------------
+import random
+import math
+x = 0
+count = 0
+num = 0
+#Don't touch -----------------
+
+# Loop
+for x in range(8):
+    print("count")
+    count = count + 1
+
+# Number multiplied
+num = int(input("This is the number to be multiplied: "));
+type(num);
+print(num * 20);
+
+# Number Divided
+num = int(input("This is the number to be divided: "));
+type(num);
+print(num / 10);
+
+# Sqrt
+num = int(input("This is the number to be square rooted: "));
+type(num);
+print(math.sqrt(num))
+
+# MOD
+num = int(input("This is the number to perform MOD on: "));
+type(num);
+print(num % 5)
